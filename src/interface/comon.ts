42174046ea39a6d82a8fe03ed89d211308ee6770
MrSohailAhmad <ahmadsohail1819@gmail.com>
2025-06-25 23:44:46 +0500

    add interfaces for user Doctor and Appointment

--- src/interface/comon.ts
+++ src/interface/comon.ts
@@ -2,3 +2,34 @@ export enum UserRole {
   USER = "user",
   ADMIN = "admin",
 }
+export interface User {
+  _id: string;
+  name: string;
+  email: string;
+  role: UserRole.ADMIN | UserRole.USER;
+  createdAt: string; // ISO string format
+}
+
+export interface Doctor {
+  _id: string;
+  name: string;
+  specialty: string;
+  availability: string[];
+  location: string;
+  contact: string;
+}
+
+export interface Appointment {
+  _id: string;
+  doctorId: {
+    name: string;
+    specialty: string;
+  };
+  userId: {
+    name: string;
+    email: string;
+  };
+  date: string;
+  time: string;
+  status: "pending" | "confirmed" | "cancelled";
+}
