e4a6a1c4f15c7afddf1d073550e02369cbafc256
MrSohailAhmad <ahmadsohail1819@gmail.com>
2025-06-27 02:31:35 +0500

    add toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import { AuthProvider } from "./context/AuthContext";
+import { ToastContainer } from "react-toastify";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <AuthProvider>
+        <ToastContainer position="top-right" autoClose={3000} />
         <AppRoutes />
       </AuthProvider>
     </BrowserRouter>
