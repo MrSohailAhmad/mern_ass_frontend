a6d8d8697b28e1c8b447a57da4d78c9126206f04
MrSohailAhmad <ahmadsohail1819@gmail.com>
2025-06-27 03:45:48 +0500

    add logout functionalty when user token is expire

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,16 +1,28 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// Auto attach token
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("accessToken");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+// Response Interceptor to handle unauthorized token
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const status = error.response?.status;
+
+    if (status === 401 || status === 403) {
+      toast.error("Session expired. Logging out...");
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("user");
+      window.location.href = "/"; // Redirect to login page
+    }
+
+    return Promise.reject(error);
   }
-  return config;
-});
+);
 
 export default axiosInstance;
